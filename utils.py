e82d9ebe2eb3b1da8501a2a30ce5abe541ce9cf3
Jimmy Kim <jkim0304@Jimmys-MacBook-Pro.local>
2019-09-24 16:38:41 -0700

    No more ping_next

--- utils.py
+++ utils.py
@@ -24,15 +24,6 @@ def check_legality(session, player, set):
     """Returns legality of player picking set as a boolean."""
     #TODO
 
-def ping_next(session):
-    """Pings the next player to pick."""
-    if session.phase == 1:
-        pass #TODO
-    elif session.phase == 2:
-        pass #TODO
-    else:
-        print('Ping being called in wrong phase.')
-
 def ctx_to_pindex(session, context):
     """Takes a context and returns the author's player index."""
     author_id = context.author.id
