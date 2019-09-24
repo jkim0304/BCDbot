106e4e84270d1965c6796bde31eec4c8a10502f8
Jimmy Kim <jimmykim@berkeley.edu>
2019-09-24 02:12:51 -0700

    implemented load/save sessions

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import classes
 import json
 
+#session JSON
 def save_session(session, path):
     """Saves session to file at path."""
     with open(path, 'w', encoding='utf-8') as f:
@@ -13,16 +14,19 @@ def load_session(path):
         sess_dict = json.load(f, cls=classes.CustomDecoder)
     return sess_dict
 
-
+#helpers
 def time_elapsed(session, player):
     """Returns the time elapsed since player's turn began."""
+    #TODO
 
-def available_sets(session, player): #TODO: add exclusive groupings so that 'player' is relevant
+def available_sets(session, player): 
     """Returns a list of sets available to this player."""
+    #TODO: add exclusive groupings so that 'player' is relevant
     return [item for item in session.sets if item not in session.taken.keys()]
 
 def check_legality(session, player, set):
     """Returns legality of player picking set as a boolean."""
+    #TODO
 
 def ping_next(session):
     """Pings the next player to pick."""
@@ -50,4 +54,5 @@ def name_to_pindex(session, name):
 
 def get_unclaimed_users_str(session: classes.Session):
     """Returns string of unclaimed users' names."""
+    #TODO
     
