cd86f68d35ebaaaf27256fa4ed7ced8c0c25e221
Jimmy Kim <jimmykim@berkeley.edu>
2020-04-02 18:14:15 -0700

    Cutting the choose_next function

--- bot.py
+++ bot.py
@@ -280,50 +280,8 @@ async def choose_set(ctx, *, arg):
         return
     next_player = sess.players[sess.curr_player]
 
-    while next_player.next_sets:
-        next_set = next_player.next_sets.pop(0)
-        if not utils.check_legality(sess, next_player, next_set):
-            await ctx.send(f'{ctx.guild.get_member(next_player.uid).mention} your queued pick is invalid. Please choose a new set.')
-            return
-        sess.taken[next_set] = next_player.name
-        next_player.sets.add(next_set)
-        utils.update_gsheet(sess, sheet, next_player.name, next_set)
-        await ctx.send(f'{next_player.name} takes {next_set}.')
-
-        phase_over = utils.increment_curr_player(sess)
-        if phase_over:
-            await ctx.send('Set draft complete. Enjoy your matches!')
-            return
-        next_player = sess.players[sess.curr_player]
-        
     next_player = ctx.guild.get_member(sess.players[sess.curr_player].uid)
     await ctx.send(f'{next_player.mention} please choose a set. (\">choose_set x\")')
-
-@bot.command(help='Locks in next sets in advance.')
-async def choose_next_sets(ctx, *, arg):
-    global sess
-    if sess == None or sess.phase != 2:
-        return
-    if utils.uid_to_pindex(sess, ctx.author.id) == sess.curr_player:
-        await ctx.send("It's your turn. Please use '>choose_set' in the main channel.")
-        return
-    pindex = utils.uid_to_pindex(sess, ctx.author.id)
-    player = sess.players[pindex]
-    set_list = [x.strip() for x in arg.split(',')]
-    player.next_sets = set_list
-
-    await ctx.send(f'Set {set_list} as your next picks.')
-
-@bot.command(help="Clears sets picked in advance with '>choose_next_sets'.")
-async def clear_next_sets(ctx):
-    global sess
-    if sess == None or sess.phase != 2:
-        return
-    pindex = utils.uid_to_pindex(sess, ctx.author.id)
-    player = sess.players[pindex]
-    player.next_sets = []
-    
-    await ctx.send("Cleared next_sets.")
 
 @bot.command(help='Gives a list of sets available to the player.')
 async def my_available_sets(ctx): 
