8409fae280f08147a7322ca9f32f9764abebc450
Geoffrey Lee <geoffreylee@geoffreys-mbp.lan>
2020-04-26 10:49:44 -0700

    gui error catching

--- gui/bcdserver.py
+++ gui/bcdserver.py
@@ -11,7 +11,9 @@ PORT = 80
 class quietServer(http.server.SimpleHTTPRequestHandler):
     def log_message(self, format, *args):
         pass
-
-with socketserver.TCPServer(("", PORT), quietServer) as httpd:
-	print("serving at port", PORT)
-	httpd.serve_forever()
+try:
+	with socketserver.TCPServer(("", PORT), quietServer) as httpd:
+		print("serving at port", PORT)
+		httpd.serve_forever()
+except Exception:
+	print(Exception)
--- runbot.sh
+++ runbot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 bot.py & 
 cd gui
-python3 bcdserver.py & 
+python3 bcdserver.py & > bcdserver.log 
+disown -a
 
